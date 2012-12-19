32361c116beea86945b19e3f8ddebee792df75ce
Bruce King <xieweiking@gmail.com>
2012-12-19 21:00:56 +0800

    v0.0.6 release

--- script/options.js
+++ script/options.js
@@ -1,4 +1,4 @@
-var MAX_RECORD_COUNT = 20; // TODO get it from options;
+var MAX_RECORD_COUNT = 60; // TODO get it from options;
 var IN_BLANK = false; // TODO get it from options;
 var STANDING = true; // TODO get it from options;
 var ALWAYS_APPEAR = true; // TODO get it from options;
